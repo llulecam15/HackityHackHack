a763205764c2d1d4571b5181a1a6b9da03fa701e
Maria Vizcaino <mariavizcaino@Marias-MacBook-Pro.local>
2016-01-29 21:53:25 -0800

    Please enter the commit message for your changes. Lines starting

---
+++ SnoozeULooze/SnoozeULooze/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnoozeULooze
+//
+//  Created by Maria Vizcaino on 1/29/16.
+//  Copyright © 2016 Maria Vizcaino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
